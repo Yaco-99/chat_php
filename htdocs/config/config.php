0c770fc85e41363f1e32194b3bbc816533656abe
Yaco-99 <coopman.yannick@gmail.com>
2020-12-18 15:39:34 +0100

    db

--- htdocs/config/config.php
+++ htdocs/config/config.php
@@ -3,7 +3,6 @@
 # define = define a case-sensitive ~constant~
 
 // Database parameters
-new PDO('mysql:host=;dbname=', '', '');
 define('DB_HOST', 'sql7.freemysqlhosting.net'); // add db host
 define('DB_USER', 'sql7382863'); // add db root
 define('DB_PASS', 'mrn39KIPpx'); // add db pass
