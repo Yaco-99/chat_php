27659ce6b23e14acbae935102864d310cb29a84b
Kultigin Karagenc <karagenc.kultigin@gmail.com>
2020-12-18 15:36:36 +0100

    button login and register on chat

--- htdocs/view/chat.php
+++ htdocs/view/chat.php
@@ -26,6 +26,7 @@ require __DIR__ . '/../controller/Users.php';
     </div>
     <div class="col-8 border border-dark p-0 window right-border">
       <div class="chat-box px-4 pb-2" id="target"></div>
+      <?php if($_SESSION['loggedin']):?>
       <div class="message-box d-flex justify-content-between">
         <input class="text-field window" type="text" id="message" maxlength="255" />
         <button id="submit">
@@ -33,6 +34,10 @@ require __DIR__ . '/../controller/Users.php';
           <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
           </svg>
         </button>
+        <?php else:?>
+          <a type="button" href="view/users/login.php" class="btn btn-primary ml-1 mb-1">Login</a>
+          <a type="button" href="view/users/register.php" class="btn btn-primary ml-1 mb-1">Register</a>
+        <?php endif;?>
       </div>
     </div>
   </div>
